ba0c9b933e2de59f12680fb55e8a931d90ce7e45
Chrisbeltran22 <chrisgonza0@gmail.com>
2020-01-12 22:46:07 -0600

    Add: new delivery menu

--- Kalvos Pizza.cpp
+++ Kalvos Pizza.cpp
@@ -6,6 +6,85 @@
 
 using namespace std; 
 
+//Option 1 function
+int deliver(){
+	
+	//Personal information variables
+	char name[20], lastN[20], address[50];
+	int phone;
+	//Menu information variables
+	int mainC, starter, drink;
+	//Money information variables
+	float amount1, amount2, amount;
+	int payment, num1, num2, num3;
+	
+	//Cleaning Buffer
+	fflush(stdin);
+	
+	//Printing personal information requirements 
+	cout<<"Nombre de la persona que realiza el pedido: ";	cin>>name;
+	cout<<endl<<"Apellido: "; cin>>lastN;
+	cout<<endl<<"Direccion: ";	cin>>address;
+	cout<<"\nTelefono: ";	cin>>phone;
+	
+	//Printing main Course menu
+	cout<<"\nPlato principal: 1> Pizza ($11.99)\t2> Ensalada ($6.99)\t3>Pasta ($8.99)\nSu seleccion: ";	cin>>mainC;
+	cout<<"Numero de platos que desea: "; cin>>num1;
+	
+	//Choosing main Course
+	switch (mainC){
+		case 1:
+			amount1=11.99*num1;
+			break;
+		case 2:
+			amount1=6.99*num1;
+			break;
+		default:
+			amount1=8.99*num1;
+			break;
+	}
+	
+	//Printing starter menu
+	cout<<"\nEntrada: 1> Cheese Rollers ($2.99, incluye 7 unidades)\t2> Pizza Hands ($1.99, incluye 5 unidades)\nSu seleccion:";	cin>>starter;
+	cout<<"Numero de entradas que desea: "; cin>>num2;
+	
+	//Choosing starter
+	switch (starter){
+		case 1:
+			amount2= amount1+ (2.99*num2);
+			break;
+		default:
+			amount2= amount1+ (1.99*num2);
+			break;
+	}
+	
+	//Printing drink menu
+	cout<<"\nBebida: 1> Gaseosa ($1.99)\t2> Te helado ($0.99)\nSu seleccion:";	cin>>drink;
+	cout<<"Numero de bebidas que desea: "; cin>>num3;
+	
+	//Choosing drink
+	switch (drink){
+		case 1:
+			amount= amount2+ (1.99*num3);
+			break;
+		default:
+			amount= amount1+ (0.99*num3);
+			break;
+	}
+	
+	//Showing total amount of the order
+	cout<<"\nMonto: $"<<amount;
+	
+	//Asking for payment method
+	cout<<endl<<"\nTipo de pago: \n1> Tarjeta\n2> Efectivo\nSu seleccion:";	cin>>payment;
+	
+	//Verifyin payment method selection
+	if (payment==1){
+		cout<<"Ha seleccionado tipo de pago con tarjeta."<<endl;
+	}
+	else cout<<"Ha seleccionado tipo de pago en efectivo."<<endl;
+}
+
 //Menu function
 int menu(){
 		
