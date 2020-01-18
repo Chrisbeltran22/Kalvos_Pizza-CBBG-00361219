a2cbd8a0834599888123a690e67ccd77534db0f0
Chris Beltrán <chrisgonza0@gmail.com>
2020-01-17 18:24:09 -0600

    Add: Change position deliver

--- Kalvos Pizza.cpp
+++ Kalvos Pizza.cpp
@@ -7,6 +7,7 @@
 using namespace std; 
 
 void menu();
+int deliver();
 
 //Option 5 function
 
@@ -103,6 +104,84 @@ int restaurant(){
 	
 }
 
+//Main function
+int main(){
+	
+	//Variables
+	char password[5];
+	string passw;
+	passw = "class"; 
+	
+	//While bucle
+	while(password != passw){
+		
+		//Printing
+		cout<<"Bienvenido, por favor introduzca la contrasena para continuar: ";
+		cin>>password;
+		
+		//Password verification
+			if (password == passw){
+				system("cls");
+				menu();
+			}
+			else cout<<"Contrasena erronea, por favor introduzca de nuevo la contrasena."<<endl;
+		
+		system ("pause");	
+		system("cls");
+	}
+}
+
+//Menu function
+void menu(){
+		
+	int option;
+	int std = 6;
+	
+	//While bucle
+	while(std !=0){
+		
+		//Printing
+		cout<<"Bienvenido a Calvo's pizza"<<endl;
+		cout<<"Seleccione el proceso a realizar: "<<endl;
+		
+		//Showing menu
+		cout<<"1> Agregar 1 pedido a domicilio\n2> Agregar 1 encargo en restaurante"<<endl;
+		cout<<"3> Ver pedidos a domicilio\n4> Ver encargos en el restaurante\n5> Ver total de venta"<<endl;	
+
+		//Selection menu
+		cin>>option;
+		
+		system("cls");
+			
+		//Menu options
+		switch (option){
+			case 1:
+				//Deliver function
+				deliver();
+				break;
+			case 2:
+				//Restaurant function
+				restaurant();
+				break; 
+			case 3:
+				//Deliveries orders
+				//seeDeliveries();
+				break;
+			case 4:
+				//Restaurant orders
+				//seeRestaurant();
+				break;
+			case 5:
+				//Total amount
+				//totalSales();
+				break;
+			default:
+				cout<<"Seleccione una entrada valida por favor"<<endl;
+				break;
+		}
+	}
+}
+
 //Option 1 function
 int deliver(){
 	
@@ -196,85 +275,4 @@ int deliver(){
 	//Cleaning screen
 	system("cls");
 }
-
-
-//Main function
-int main(){
-	
-	//Variables
-	char password[5];
-	string passw;
-	passw = "class"; 
-	
-	//While bucle
-	while(password != passw){
-		
-		//Printing
-		cout<<"Bienvenido, por favor introduzca la contrasena para continuar: ";
-		cin>>password;
-		
-		//Password verification
-			if (password == passw){
-				system("cls");
-				menu();
-			}
-			else cout<<"Contrasena erronea, por favor introduzca de nuevo la contrasena."<<endl;
-		
-		system ("pause");	
-		system("cls");
-	}
-}
-
-//Menu function
-void menu(){
-		
-	int option;
-	int std = 6;
-	
-	//While bucle
-	while(std !=0){
-		
-		//Printing
-		cout<<"Bienvenido a Calvo's pizza"<<endl;
-		cout<<"Seleccione el proceso a realizar: "<<endl;
-		
-		//Showing menu
-		cout<<"1> Agregar 1 pedido a domicilio\n2> Agregar 1 encargo en restaurante"<<endl;
-		cout<<"3> Ver pedidos a domicilio\n4> Ver encargos en el restaurante\n5> Ver total de venta"<<endl;	
-
-		//Selection menu
-		cin>>option;
-		
-		system("cls");
-			
-		//Menu options
-		switch (option){
-			case 1:
-				//Deliver function
-				deliver();
-				break;
-			case 2:
-				//Restaurant function
-				restaurant();
-				break; 
-			case 3:
-				//Deliveries orders
-				//seeDeliveries();
-				break;
-			case 4:
-				//Restaurant orders
-				//seeRestaurant();
-				break;
-			case 5:
-				//Total amount
-				//totalSales();
-				break;
-			default:
-				cout<<"Seleccione una entrada valida por favor"<<endl;
-				break;
-		}
-	}
-}
-
-
 
